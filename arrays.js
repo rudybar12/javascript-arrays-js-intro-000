3daf43baa195ebf402c9d184ea9437b35898a082
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-11 05:39:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  return array[(index)]
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
