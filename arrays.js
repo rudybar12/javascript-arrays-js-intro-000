14d166511642d28b7749419eb970f122f7764dec
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-11 05:15:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array,element){
   return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfAray(array, element){
+function destructivelyAddElementToBeginingOfAray(array, element){
   array.unshift(element)
   return array;
 }
