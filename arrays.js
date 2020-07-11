94660e9d7906cc11e8c6948d8a9775b516f7e9cb
rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-11 05:40:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
+  return array.slice(-2);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
